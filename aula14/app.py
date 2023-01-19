4345be48cc2ea6f82d8298a1b9bc2ece4b297a93
joao-rcosta19 <joao.rcosta19@gmail.com>
2023-01-19 19:18:19 -0300

    atualização da aula 14

--- aula14/app.py
+++ aula14/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
 app = Flask(__name__, template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///estudantes.sqlite3'
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
-class Estudantes(db.Model):
+class Estudante(db.Model):
     id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(150))
     idade = db.Column(db.Integer)
@@ -15,12 +17,42 @@ class Estudantes(db.Model):
         self.nome = nome
         self.idade = idade
 
+with app.app_context():
+    db.create_all()
+
 
 @app.route('/')
 def index():
-    estudantes = Estudantes.query.all()
+    estudantes = Estudante.query.all()
     return render_template('index.html', estudantes=estudantes)
 
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        estudante = Estudante(request.form['nome'], request.form['idade'])
+        db.session.add(estudante)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add.html')
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    estudante = Estudante.query.get(id)
+    if request.method == 'POST':
+        estudante.nome = request.form['nome']
+        estudante.idade = request.form['idade']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('edit.html', estudante=estudante)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    estudante = Estudante.query.get(id)
+    db.session.delete(estudante)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
-    db.create_all()
     app.run(debug=True)
